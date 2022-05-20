9fe29d550deeafa50be14d0a025705b7c20f71cc
fada <johnsonkwabenadarko@Air-von-DAVID.fritz.box>
2022-05-20 16:12:39 +0200

    make it better

--- server/index.js
+++ server/index.js
@@ -255,6 +255,6 @@ const PORT = process.env.PORT || 8000
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 )
