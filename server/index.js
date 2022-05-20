ea790dddfe847de567ee4c9b926a682b5d1ab02b
fada <johnsonkwabenadarko@Air-von-DAVID.fritz.box>
2022-05-20 23:49:54 +0200

    make it better

--- server/index.js
+++ server/index.js
@@ -269,6 +269,15 @@ app.post('/message', async (req, res) => {
         await client.close()
     }
 })
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("build"));
+  
+  app.get('*', (req, res) => {
+     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+  
+  }
+ 
 
 const PORT = process.env.PORT || 8000
 
